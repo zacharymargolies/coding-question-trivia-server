33f2151b9af3c0b3b5279480d4c62da3c1cd17f9
Zach Margolies <zachary.margolies@gmail.com>
2018-12-08 12:38:11 -0500

    seed file funcitonal

--- script/seed.js
+++ script/seed.js
@@ -1,18 +1,210 @@
 'use strict'
 
 const db = require('../server/db')
-const {User} = require('../server/db/models')
+const {Fact, Topic, Question, Answer} = require('../server/db/models')
+
+const newFacts = [
+  {
+    topicId: 1,
+    content: 'HTML is short for Hypertext Markup Language.',
+    difficulty: 0.3,
+    daysBetweenReveiws: 1,
+    dateLastReviewd: null,
+    quizzable: false,
+    discard: false,
+    docsLink: null
+  },
+  {
+    topicId: 1,
+    content: 'HTML can be considered the content of our website.',
+    difficulty: 0.3,
+    daysBetweenReveiws: 1,
+    dateLastReviewd: null,
+    quizzable: false,
+    discard: false,
+    docsLink: null
+  },
+  {
+    topicId: 1,
+    content: 'HTML elements are the building blocks of HTML pages.',
+    difficulty: 0.3,
+    daysBetweenReveiws: 1,
+    dateLastReviewd: null,
+    quizzable: false,
+    discard: false,
+    docsLink: null
+  },
+  {
+    topicId: 1,
+    content: 'HTML elements are represented by tags.',
+    difficulty: 0.3,
+    daysBetweenReveiws: 1,
+    dateLastReviewd: null,
+    quizzable: false,
+    discard: false,
+    docsLink: null
+  },
+  {
+    topicId: 1,
+    content:
+      'HTML tags label pieces of content such as "heading", "paragraph", "table", and so on.',
+    difficulty: 0.3,
+    daysBetweenReveiws: 1,
+    dateLastReviewd: null,
+    quizzable: false,
+    discard: false,
+    docsLink: null
+  },
+  {
+    topicId: 1,
+    content:
+      'Browsers do not display HTML tags, but use them to render the content of the page.',
+    difficulty: 0.3,
+    daysBetweenReveiws: 1,
+    dateLastReviewd: null,
+    quizzable: false,
+    discard: false,
+    docsLink: null
+  },
+  {
+    topicId: 2,
+    content: 'HTML can be considered the content of our website.',
+    difficulty: 0.3,
+    daysBetweenReveiws: 1,
+    dateLastReviewd: null,
+    quizzable: false,
+    discard: false,
+    docsLink: null
+  },
+  {
+    topicId: 3,
+    content: 'CSS stands for Cascading Style Sheets.',
+    difficulty: 0.3,
+    daysBetweenReveiws: 1,
+    dateLastReviewd: null,
+    quizzable: false,
+    discard: false,
+    docsLink: null
+  },
+  {
+    topicId: 3,
+    content:
+      'CSS provides the styling for our websites. It describes how the HTML elements in our websites should be displayed.',
+    difficulty: 0.3,
+    daysBetweenReveiws: 1,
+    dateLastReviewd: null,
+    quizzable: false,
+    discard: false,
+    docsLink: null
+  },
+  {
+    topicId: 3,
+    content:
+      'CSS is a declarative language. This means that CSS explicitly describes its desired results. This means the language provides what is done, rather than how to do it.',
+    difficulty: 0.3,
+    daysBetweenReveiws: 1,
+    dateLastReviewd: null,
+    quizzable: false,
+    discard: false,
+    docsLink: null
+  }
+]
+
+const newTopics = [
+  {
+    main: 'HTML',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/1200px-HTML5_logo_and_wordmark.svg.png',
+    topicId: null
+  },
+  {
+    main: 'CSS',
+    image: 'http://www.growingwiththeweb.com/images/general/css3.png',
+    topicId: null
+  },
+  {
+    main: 'Javascript',
+    image: 'https://c1.staticflickr.com/4/3701/19224697601_6b600f21eb.jpg',
+    topicId: null
+  },
+  {
+    main: 'NodeJS',
+    image:
+      'https://seeklogo.com/images/N/nodejs-logo-FBE122E377-seeklogo.com.png',
+    topicId: null
+  },
+  {
+    main: 'ExpressJS',
+    image: 'http://nashvillesoftwareschool.com/images/technologies/express.png',
+    topicId: null
+  },
+  {
+    main: 'ReactJS',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/640px-React-icon.svg.png',
+    topicId: null
+  },
+  {
+    main: 'ReduxJS',
+    image: 'https://avatars0.githubusercontent.com/u/13142323?s=400&v=4',
+    topicId: null
+  }
+]
+
+const newQuestions = [
+  {
+    factId: 1,
+    topicId: 1,
+    content: 'What does HTML stand for?',
+    correctAnswer: 'Hypertext Markup Language',
+    difficulty: 0.3,
+    type: null
+  },
+  {
+    factId: 2,
+    topicId: 1,
+    content: 'What creates the structure for the content of our website?',
+    correctAnswer: 'HTML',
+    difficulty: 0.3,
+    type: null
+  },
+  {
+    factId: 7,
+    topicId: 2,
+    content: 'What does CSS stand for?',
+    correctAnswer: 'Cascading Style Sheets',
+    difficulty: 0.3,
+    type: null
+  },
+  {
+    factId: 8,
+    topicId: 2,
+    content: 'What provides the styling for a website?',
+    correctAnswer: 'CSS',
+    difficulty: 0.3,
+    type: null
+  }
+]
+
+const newAnswers = [
+  {
+    value: 'What does HTML stand for?',
+    type: 'Multiple Choice',
+    questionId: 1
+  }
+]
 
 async function seed() {
   await db.sync({force: true})
   console.log('db synced!')
 
-  const users = await Promise.all([
-    User.create({email: 'cody@email.com', password: '123'}),
-    User.create({email: 'murphy@email.com', password: '123'})
-  ])
+  await Promise.all(
+    newTopics.map(topic => Topic.create(topic)),
+    newFacts.map(fact => Fact.create(fact)),
+    newQuestions.map(question => Question.create(question)),
+    newAnswers.map(answer => Answer.create(answer))
+  )
 
-  console.log(`seeded ${users.length} users`)
   console.log(`seeded successfully`)
 }
 
--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,31 +1,32 @@
 // TODO: Require models here, define associations, export models
-const Fact = require('./fact');
-const Topic = require('./topic');
-const Question = require('./question');
-const Answer = require('./answer.js');
+const Fact = require('./fact')
+const Topic = require('./topic')
+const Question = require('./question')
+const Answer = require('./answer')
 
 // Many facts belong to any Topic: Topic --> fact, fact, fact
-Topic.hasMany(Fact);
-Fact.belongsTo(Topic);
+Topic.hasMany(Fact)
+Fact.belongsTo(Topic)
 
 // A fact has many questions: Fact --> question, question, question
-Fact.hasMany(Question);
-Question.belongsTo(Fact);
+Fact.hasMany(Question)
+Question.belongsTo(Fact)
 
 // A topic has many question: Topic --> question, question, question
-Topic.hasMany(Question);
-Question.belongsTo(Topic);
+Topic.hasMany(Question)
+Question.belongsTo(Topic)
 
 // Topics may have sub-topics: Topic --> topic, topic, topic
-Topic.hasMany(Topic);
-Topic.belongsTo(Topic);
+Topic.hasMany(Topic)
+Topic.belongsTo(Topic)
 
 // Questions have one correct answer: Question --> correct answer, false answer, false answer
-Question.hasOne(Answer);
-Answer.belongsTo(Question);
+Question.hasOne(Answer)
+Answer.belongsTo(Question)
 
 module.exports = {
   Fact,
   Topic,
-  Question
-};
+  Question,
+  Answer
+}
